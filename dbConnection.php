4032a6bd642d953d999e923c3d2b88fa3566df0f
Raju Prajapati <48856985+rajuprajapati4@users.noreply.github.com>
2020-04-17 19:50:38 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,5 +1,5 @@
 <?php
 //all the variables defined here are accessible in all the files that include this one
-$con= new mysqli('sql12.freesqldatabase.com','sql12330712','KHuj4seHq4','sql12330712')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('sql12.freesqldatabase.com','sql12334153','aiQHaDKC7U','sql12334153')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
